76ae537fe576dd9f1b8550a62e78d0bb0b760d02
linderlu <a01036049948@gmail.com>
2026-08-14 13:51:10 +0900

    s

--- src/data/chapters/chapter-03-ending.ts
+++ src/data/chapters/chapter-03-ending.ts
@@ -12,7 +12,7 @@ export const chapterEndingScenes: GameScene[] = [
     id: "coming-soon",
     sceneType: "ending",
     image: EGGSY,
-    body: "..이...\n...자식..\n일어나라! 이놈자식!!!\n\n수업시간에 자빠져 자고있어?\n\n{name}:앗...꿈?\n나가서 내 담배나 사와! ",
+    body: "..이...\n...자식..\n일어나라! 이놈자식!!!\n\n수업시간에 자빠져 자고있어?\n\n{name}:앗...꿈?\n\n 나가서 내 담배나 사와!\n\n 네..넵!...\n (ㅈ같은꿈이군..) \n\n -The End-",
     interactionType: "message",
   },
 ];
